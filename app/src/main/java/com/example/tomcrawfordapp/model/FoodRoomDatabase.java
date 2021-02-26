928213dfab7545128786bdd2e2fbe088b89ee42c
tomcrawford98 <tomcrawford98@gmail.com>
2021-02-26 17:22:08 +0000

    9th commit

--- app/src/main/java/com/example/tomcrawfordapp/model/FoodRoomDatabase.java
+++ app/src/main/java/com/example/tomcrawfordapp/model/FoodRoomDatabase.java
@@ -1,10 +1,14 @@
 package com.example.tomcrawfordapp.model;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
 @Database(entities = {Food.class}, version = 1, exportSchema = false)
 
 public abstract class FoodRoomDatabase extends RoomDatabase {
@@ -21,10 +25,46 @@ public abstract class FoodRoomDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             FoodRoomDatabase.class, "food_database")
                             .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback(){
+
+                @Override
+                public void onOpen (@NonNull SupportSQLiteDatabase db){
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final FoodDAO mDao;
+        String[] foods = {"Full English", "Lasagna", "Fillet Steak"};
+
+        PopulateDbAsync(FoodRoomDatabase db) {
+            mDao = db.foodDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate the database
+            // when it is first created
+            mDao.deleteAll();
+
+            for (int i = 0; i <= foods.length - 1; i++) {
+
+                Food food = new Food(foods[i]);
+                mDao.insert(food);
+            }
+            return null;
+        }
     }
 }
